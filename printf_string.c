c850b126b7634c9c78511367406115700118cb8a
Gyamps <dentukwabena.kg@gmail.com>
2023-03-27 17:57:28 +0000

    make code cleaner

--- printf_string.c
+++ printf_string.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
-
 int printf_string(va_list val)
 {
     char *s;
     int i, len;
 
     s = va_arg(val, char *);
-    if(s == NULL)
+
+    /**
+     * Looked at this first conditional again and I still
+     * don't get the reason for this. If it's null, why not
+     * return 0 then??
+     */
+    if (s == NULL)
     {
         s = "null";
         len = _strlen(s);
-        for(i=0; i< len; i++)
+        for (i = 0; i < len; i++)
             _putchar(s[i]);
-        return(len);
+        return (len);
     }
     else
     {
         len = _strlen(s);
-        for(i=0; i <len; i++)
+        for (i = 0; i < len; i++)
             _putchar(s[i]);
         return (len);
     }
-    
 }
