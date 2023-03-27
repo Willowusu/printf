c781cd4f7ec55c9ab98c296ec97c2344e70b9abb
Gyamps <dentukwabena.kg@gmail.com>
2023-03-27 22:29:23 +0000

    answer ot question 4

--- buffer_1024.c
+++ buffer_1024.c
@@ -1,29 +1,30 @@
-//declare buffer 1024
-//"the quick brown fox jumps over the lazy dog"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int _putchar(char c)
+void my_putchar(char *c)
 {
-    static char buffer[1024];
-    static int i;
+    int len = strlen(c);
+    int i = 0, num_bytes = 0;
 
-    if(c == -1 || i >= 1024)
-    {
-        write(1, &buffer, i);
-        i = 0;
-    }
-    if(c != -1)
-    {
-        buffer[i] = c;
-        i++;
-    }
+    char buffer[1024];
 
-    return (1);
+    while (i < len)
+    {
+        num_bytes = len - i < 1024 ? len - i : 1024;
+        strncpy(buffer + i, &c[i], num_bytes);
+        write(1, buffer, num_bytes);
+        i += num_bytes;
+    }
 }
 
-int putting(char *str)
+
+int main(void)
 {
-    int i;
-    for(i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
-    return (i);
+    char *a = "The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog\n";
+
+    my_putchar(a);
+
+    return (0);
 }
