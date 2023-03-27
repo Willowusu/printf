71dc7a91f36fe85c7047a3ab1939acdb3cba1187
Gyamps <dentukwabena.kg@gmail.com>
2023-03-27 17:50:16 +0000

    Added more test cases for s, d and i

--- main.c
+++ main.c
@@ -1,22 +1,33 @@
-#include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
-
-
 /**
- * main - Entry point
+ * main - Entry point of program
  *
- * Return: Always 0
+ * Return: always 0
  */
 int main(void)
 {
-    int len;
 
+    char c[] = "Let's try to printf a simple sentence.\n";
+    _printf("%s\n", c);
+    _printf("Positive int. number with [d]: %d\n", 42);
+    _printf("Negative int. number with [d]: %d\n", -42);
 
-    len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("Positive int. number with [i]: %i\n", 42);
+    _printf("Negative int. number with [i]: %i\n\n", -42);
 
-     _printf("Length:[%d, %i]\n", len, len);
+    _printf("Positive octal number with [d]: %d\n", 052);
+    _printf("Negative octal number with [d]: %d\n", -052);
+
+    _printf("Positive octal number with [i]: %i\n", 052);
+    _printf("Negative octal number with [i]: %i\n\n", -052);
+
+    _printf("Positive hex number with [d]: %d\n", 0x2A);
+    _printf("Negative hex number with [d]: %d\n", -0x2A);
+
+    _printf("Positive hex number with [i]: %i\n", 0x2A);
+    _printf("Negative hex number with [i]: %i\n", -0x2A);
 
     return (0);
 }
